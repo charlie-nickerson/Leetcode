50dc361fbcd1b7c4ec1c583aefccc366d7ef6d38
charlie-nickerson <charlie@nickerson.com>
2025-01-30 15:55:14 -0800

    Added the difficulty to problem 128

--- leetcode128.py
+++ leetcode128.py
@@ -1,5 +1,5 @@
 # Longest Consecutive Sequence
-
+# Diffulty: Medium
 # Description: Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 # You must write an algorithm that runs in O(n) time.
 
